919dda5b06298e48b5658f816d2bfc4cd1520899
vpugh <hello@toripugh.com>
2020-05-24 00:22:57 -0400

    Add background to about and work , clean up styling on blog

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,15 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import {AboutContainer, AvatarContainer, Quote } from '../styles/about-styled';
+import { AboutContainer, AvatarContainer, Quote } from "../styles/about-styled"
 
 const AboutPage = () => (
   <Layout>
     <SEO title="UI/UX Developer" />
-    <section className="about">
+    <section
+      className="about"
+      style={{ background: "#fff", padding: "30px 50px", marginTop: 40 }}
+    >
       <AvatarContainer>
         <div className="avatar--grid">
           <div className="avatar--img" />
@@ -15,38 +18,140 @@ const AboutPage = () => (
           </p>
         </div>
         <h2 className="about--title">
-        Driven by curiosity and a desire to make things work better.
+          Driven by curiosity and a desire to make things work better.
         </h2>
         <a className="btn block" href="../tpr.pdf" download="tori-pugh-resume">
           Download Resume
         </a>
       </AvatarContainer>
-      <Quote>I listen to understand a problem, do research to identify and/or confirm it, and then create solutions for them.</Quote>
+      <Quote>
+        I listen to understand a problem, do research to identify and/or confirm
+        it, and then create solutions for them.
+      </Quote>
       <AboutContainer>
         <div className="text-container">
           <p>
-            I’m a UI/UX developer who is passionate about creating products that puts the users first. I want to make products that run better and are easier for the user. I've had varied involvement as a visual designer, front-end developer, or email developer. I think there should always be an aspect of user-experience understanding in design, development, or email creation.
+            I’m a UI/UX developer who is passionate about creating products that
+            puts the users first. I want to make products that run better and
+            are easier for the user. I've had varied involvement as a visual
+            designer, front-end developer, or email developer. I think there
+            should always be an aspect of user-experience understanding in
+            design, development, or email creation.
           </p>
           <p>
-            The idea — “design is more then making it pretty” — is my driving principle. Design is the culmination of creation and execution of a solution; understanding your audience; and critical thinking skills to solve problems. This combination of thought and execution is what I find most enjoyable and rewarding.
+            The idea — “design is more then making it pretty” — is my driving
+            principle. Design is the culmination of creation and execution of a
+            solution; understanding your audience; and critical thinking skills
+            to solve problems. This combination of thought and execution is what
+            I find most enjoyable and rewarding.
           </p>
           <p>
-            My greatest asset is my eternal passion for learning, due in part to my constant curiosity. I can learn new things at amazing speeds. I take great enjoyment in trying to find more efficient ways to create things and upcoming technologies. I’m always looking to expand my knowledge through online learning, classes, going to conferences, or reading articles.
+            My greatest asset is my eternal passion for learning, due in part to
+            my constant curiosity. I can learn new things at amazing speeds. I
+            take great enjoyment in trying to find more efficient ways to create
+            things and upcoming technologies. I’m always looking to expand my
+            knowledge through online learning, classes, going to conferences, or
+            reading articles.
           </p>
           <p>
-            I share and interact on <a className="inline-link" href="https://twitter.com/teekatwo" rel="noopener noreferrer" target="_blank">Twitter</a>; get inspired and showcase work on <a className="inline-link" href="https://dribbble.com/teekatwo" rel="noopener noreferrer" target="_blank">Dribbble</a>; tinker and learn new code on <a className="inline-link" href="https://codepen.io/Vpugh/" rel="noopener noreferrer" target="_blank">Codepen</a>; work on side projects on <a className="inline-link" href="https://github.com/vpugh" rel="noopener noreferrer" target="_blank">Github</a>; and write when I can on <a className="inline-link" href="https://medium.com/@teekatwo" rel="noopener noreferrer" target="_blank">Medium</a> or <a className="inline-link" href="https://dev.to/teekatwo" rel="noopener noreferrer" target="_blank">Dev.to</a>. In my free time, I'm an avid reader with a ever-growing backlog of books or trying to get through my game collection for the PS4 (Red Dead Redemption 2, AC Odyssey, Tomb Raider).
+            I share and interact on{" "}
+            <a
+              className="inline-link"
+              href="https://twitter.com/teekatwo"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Twitter
+            </a>
+            ; get inspired and showcase work on{" "}
+            <a
+              className="inline-link"
+              href="https://dribbble.com/teekatwo"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Dribbble
+            </a>
+            ; tinker and learn new code on{" "}
+            <a
+              className="inline-link"
+              href="https://codepen.io/Vpugh/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Codepen
+            </a>
+            ; work on side projects on{" "}
+            <a
+              className="inline-link"
+              href="https://github.com/vpugh"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Github
+            </a>
+            ; and write when I can on{" "}
+            <a
+              className="inline-link"
+              href="https://medium.com/@teekatwo"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Medium
+            </a>{" "}
+            or{" "}
+            <a
+              className="inline-link"
+              href="https://dev.to/teekatwo"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Dev.to
+            </a>
+            . In my free time, I'm an avid reader with a ever-growing backlog of
+            books or trying to get through my game collection for the PS4 (Red
+            Dead Redemption 2, AC Odyssey, Tomb Raider).
           </p>
           <p>
-            I've had two speaking engagements at <a className="inline-link" href="https://www.youtube.com/watch?v=xHpZzXvwjwM&amp;list=PLyLTyCCJDTTe5Ay6yOqiBW8KgZyKXRI5U&amp;index=14" rel="noopener noreferrer" target="_blank">Codeland 2018</a> and <a className="inline-link" href="https://wordpress.tv/2018/12/05/tori-pugh-the-importance-of-typography-design-usability-cost-and-loading-times/" rel="noopener noreferrer" target="_blank">WordCamp Philly 2018</a>.
+            I've had two speaking engagements at{" "}
+            <a
+              className="inline-link"
+              href="https://www.youtube.com/watch?v=xHpZzXvwjwM&amp;list=PLyLTyCCJDTTe5Ay6yOqiBW8KgZyKXRI5U&amp;index=14"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              Codeland 2018
+            </a>{" "}
+            and{" "}
+            <a
+              className="inline-link"
+              href="https://wordpress.tv/2018/12/05/tori-pugh-the-importance-of-typography-design-usability-cost-and-loading-times/"
+              rel="noopener noreferrer"
+              target="_blank"
+            >
+              WordCamp Philly 2018
+            </a>
+            .
           </p>
         </div>
         <div className="sidebar">
           <h3>Development</h3>
-          <p>HTML, CSS, SASS, Javascript, Git, Gulp, NodeJs, Vue, React, Jest, Gatsby <span className="learning">Wordpress</span></p>
+          <p>
+            HTML, CSS, SASS, Javascript, Git, Gulp, NodeJs, Vue, React, Jest,
+            Gatsby <span className="learning">Wordpress</span>
+          </p>
           <h3>Design</h3>
-          <p>Responsive Web Design, UX/UI, Interaction Design, Typography, Branding, Prototyping, Email Design, Design Strategy</p>
+          <p>
+            Responsive Web Design, UX/UI, Interaction Design, Typography,
+            Branding, Prototyping, Email Design, Design Strategy
+          </p>
           <h3>Tools</h3>
-          <p>Sketch, Photoshop, Illustrator, InDesign, Dreamweaver, Coda, Sublime Text, GitHub, VSCode, Webstorm, SendGrid, <span className="learning">MailChimp</span>, <span className="learning">Affinity Designer</span></p>
+          <p>
+            Sketch, Photoshop, Illustrator, InDesign, Dreamweaver, Coda, Sublime
+            Text, GitHub, VSCode, Webstorm, SendGrid,{" "}
+            <span className="learning">MailChimp</span>,{" "}
+            <span className="learning">Affinity Designer</span>
+          </p>
         </div>
       </AboutContainer>
     </section>
--- src/templates/blog-template.js
+++ src/templates/blog-template.js
@@ -1,104 +1,120 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
-import { graphql, Link } from 'gatsby'
-import HomeBlog from "../components/home-blog";
-import { BlogContainer, BlogHome } from '../styles/blog-styled';
+import { graphql, Link } from "gatsby"
+import HomeBlog from "../components/home-blog"
+import { BlogContainer, BlogHome } from "../styles/blog-styled"
 
 const IndexPage = ({ data, pageContext }) => {
-  const { currentPage, isFirstPage, isLastPage, totalPages } = pageContext;
-  const nextPage = `/blog/${String(currentPage + 1)}`;
-  const prevPage = currentPage === 2 ? '/blog' : `/blog/${String(currentPage - 1)}`
+  const { currentPage, isFirstPage, isLastPage, totalPages } = pageContext
+  const nextPage = `/blog/${String(currentPage + 1)}`
+  const prevPage =
+    currentPage === 2 ? "/blog" : `/blog/${String(currentPage - 1)}`
 
   return (
-  <Layout>
-    <SEO title="Blog" />
-    <BlogContainer>
-      <h2 className="title" style={{ padding: '10px 0' }}>Writings and Mental Musings</h2>
-      <BlogHome>
-        {data.allMarkdownRemark.edges.map(({ node: post}) => {
-          const title = post.frontmatter.title;
-          const tags = post.frontmatter.tags;
-          const fluidImage = post.frontmatter.cover_image ? post.frontmatter.cover_image.childImageSharp.fluid : '../images/';
-          const image = post.frontmatter.cover_image;
-          const date = post.frontmatter.date;
-          const slug = post.frontmatter.path;
-          const id = post.id;
-          const timeToRead = post.timeToRead;
+    <Layout>
+      <SEO title="Blog" />
+      <BlogContainer>
+        <h2
+          className="title"
+          style={{ padding: "10px 0", color: "rgba(255, 255, 255, 0.9" }}
+        >
+          Articles | Writings and Mental Musings
+        </h2>
+        <BlogHome>
+          {data.allMarkdownRemark.edges.map(({ node: post }) => {
+            const title = post.frontmatter.title
+            const tags = post.frontmatter.tags
+            const fluidImage = post.frontmatter.cover_image
+              ? post.frontmatter.cover_image.childImageSharp.fluid
+              : "../images/"
+            const image = post.frontmatter.cover_image
+            const date = post.frontmatter.date
+            const slug = post.frontmatter.path
+            const id = post.id
+            const timeToRead = post.timeToRead
 
-          return (
-            <HomeBlog slug={slug} image={image} title={title} date={date} tags={tags} timeToRead={timeToRead} key={id} fluidImage={fluidImage} />
-          )
-        })}
-      </BlogHome>
-      <div style={{
-        display: 'flex',
-        alignItems: 'center',
-        justifyContent: 'space-around',
-        maxWidth: 500,
-        margin: '40px auto 20px auto',
-        borderTop: '1px solid #ddd',
-        paddingTop: 20,
-      }}>
-        {!isFirstPage && (
-          <Link to={prevPage} rel="prev">
-            Prev Page
-          </Link>
-        )}
-        {Array.from({ length: totalPages }, (_, index) => (
-            <Link key={index} to={`/blog${index === 0 ? '' : `/${index + 1}`}`} style={{ fontWeight: currentPage === index + 1 ? 'bold': '' }}>
+            return (
+              <HomeBlog
+                slug={slug}
+                image={image}
+                title={title}
+                date={date}
+                tags={tags}
+                timeToRead={timeToRead}
+                key={id}
+                fluidImage={fluidImage}
+              />
+            )
+          })}
+        </BlogHome>
+        <div
+          style={{
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "space-around",
+            maxWidth: 500,
+            margin: "40px auto 20px auto",
+            borderTop: "1px solid rgba(255,255,255,0.2)",
+            paddingTop: 20,
+            color: "rgba(255, 255, 255, 0.4)",
+          }}
+        >
+          {!isFirstPage && (
+            <Link to={prevPage} rel="prev">
+              Prev Page
+            </Link>
+          )}
+          {Array.from({ length: totalPages }, (_, index) => (
+            <Link
+              key={index}
+              to={`/blog${index === 0 ? "" : `/${index + 1}`}`}
+              style={{ fontWeight: currentPage === index + 1 ? "bold" : "" }}
+            >
               {index + 1}
             </Link>
           ))}
-        {!isLastPage && (
-          <Link to={nextPage} rel="next">
-            Next Page
-          </Link>
-        )}
-      </div>
-    </BlogContainer>
-  </Layout>
+          {!isLastPage && (
+            <Link to={nextPage} rel="next">
+              Next Page
+            </Link>
+          )}
+        </div>
+      </BlogContainer>
+    </Layout>
   )
 }
 
 export default IndexPage
 
 export const blogQuery = graphql`
-query($skip: Int!, $limit: Int!){
-  allMarkdownRemark(
-    filter: {
-      frontmatter: {
-        draft: {eq: false},
-        date: {nin: "null"},
-      },
-    },
-    sort: {
-      fields: frontmatter___date,
-      order: DESC
-    },
-    skip: $skip,
-    limit: $limit,
+  query($skip: Int!, $limit: Int!) {
+    allMarkdownRemark(
+      filter: { frontmatter: { draft: { eq: false }, date: { nin: "null" } } }
+      sort: { fields: frontmatter___date, order: DESC }
+      skip: $skip
+      limit: $limit
     ) {
-    edges {
-      node {
-        id
-        frontmatter {
-          path
-          excerpt
-          title
-          tags
-          cover_image {
-            childImageSharp {
-              fluid {
-                ...GatsbyImageSharpFluid
+      edges {
+        node {
+          id
+          frontmatter {
+            path
+            excerpt
+            title
+            tags
+            cover_image {
+              childImageSharp {
+                fluid {
+                  ...GatsbyImageSharpFluid
+                }
               }
             }
+            date(formatString: "MMMM Do, YYYY")
           }
-          date(formatString: "MMMM Do, YYYY")
+          timeToRead
         }
-        timeToRead
       }
     }
   }
-}
 `
