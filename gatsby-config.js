cb5d1a51f541d161a513dc74b2ad7738d8c3b81f
vpugh <hello@toripugh.com>
2020-08-22 11:12:38 -0400

    Revert typography change

--- gatsby-config.js
+++ gatsby-config.js
@@ -69,7 +69,6 @@ module.exports = {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
-        omitGoogleFont: true,
       },
     },
     {
